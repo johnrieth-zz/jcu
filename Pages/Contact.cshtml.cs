f8bd32fe5f03de91b894c0f83c42f29ae698f0ea
johnrieth <john.rieth@outlook.com>
2018-04-23 12:22:39 -0700

    site

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace jcu.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email {get; set;}
+
+        [Required]
+        [Phone]
+        [Display(Name="Phone")]
+        public string Phone {get; set;}
+    }
+}
